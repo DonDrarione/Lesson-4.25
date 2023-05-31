bc6197752b778a9b13dae67090838cc34deeef6e
sunny <sunny@DESKTOP-UJ1BUSP>
2023-05-31 21:59:39 +0300

    Добавьте файлы проекта.

---
+++ Lesson 4.25/Program.cs
@@ -0,0 +1,8 @@
+﻿var A = Convert.ToInt32 (Console.ReadLine ()); 
+var B  = Convert.ToInt32 (Console.ReadLine ());
+var C = 1;
+for (int i = 0; i < B; i++) //возведение в степень это 1 умноженное A B раз
+{
+    C *= A;
+}
+Console.WriteLine(C);
